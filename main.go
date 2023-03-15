376122b4b651d8420d1600a1f8a9d54eadec971a
2xic <5640782+2xic@users.noreply.github.com>
2023-03-15 13:54:31 +0100

    Added connection options env variable

--- main.go
+++ main.go
@@ -1,56 +1,78 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "os"
-    "database/sql"
-    _ "github.com/nakagami/firebirdsql"
-    "github.com/joho/godotenv"
-    "strings"
+	"database/sql"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/nakagami/firebirdsql"
 )
 
 type SqlParams struct {
-    Sql     string   `json:"sql"`
-    Params  []interface{} `json:"params"`
+	Sql    string        `json:"sql"`
+	Params []interface{} `json:"params"`
+}
+
+func rawExecuteSql(
+	params SqlParams,
+) map[string][]interface{} {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	USERNAME := os.Getenv("USERNAME")
+	PASSWORD := os.Getenv("PASSWORD")
+	HOST := os.Getenv("HOST")
+	PORT := os.Getenv("PORT")
+	DATABASE := os.Getenv("DATABASE")
+	CONNECTION_OPTIONS := os.Getenv("CONNECTION_OPTIONS")
+
+	connectionString := strings.Join([]string{
+		USERNAME, ":", PASSWORD, "@", HOST, ":", PORT, DATABASE,
+		CONNECTION_OPTIONS,
+	}, "")
+
+	var db *sql.DB
+	db, err = sql.Open("firebirdsql", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query(params.Sql, params.Params...)
+	if err != nil {
+		panic(err)
+	}
+	row_json, err := SQLToJSON(rows)
+	if err != nil {
+		panic(err)
+	}
+	return row_json
 }
 
 func executeSql(c *gin.Context) {
-    var params SqlParams
+	var params SqlParams
 
-    if err := c.BindJSON(&params); err != nil {
-        return
-    }
+	if err := c.BindJSON(&params); err != nil {
+		return
+	}
 
-    err := godotenv.Load()
-    if err != nil {
-      panic("Error loading .env file")
-    }
+	row_json := rawExecuteSql(params)
 
-    USERNAME := os.Getenv("USERNAME")
-    PASSWORD := os.Getenv("PASSWORD")
-    HOST := os.Getenv("HOST")
-    PORT := os.Getenv("PORT")
-    DATABASE := os.Getenv("DATABASE")
+	c.JSON(200, row_json)
+}
 
-    connectionString := strings.Join([]string{
-        USERNAME, ":", PASSWORD, "@", HOST, ":", PORT, "/", DATABASE,
-    }, "")
-  
-    var db *sql.DB
-    db, _ = sql.Open("firebirdsql", connectionString)
-    rows, err := db.Query(params.Sql, params.Params...)
-    if err != nil {
-        panic(err);
-    }
-    row_json, _ := SQLToJSON(rows)
-    c.JSON(200, row_json)
+func sayHello(c *gin.Context) {
+	c.String(http.StatusOK, "hello world")
 }
 
 func main() {
-//    router := gin.Default()
-    router := gin.New()
-    router.POST("/", executeSql)
+	router := gin.New()
+	router.GET("/", sayHello)
+	router.POST("/", executeSql)
 
-//    router.Run("localhost:8080")
-    router.Run("0.0.0.0:8080")
+	router.Run("0.0.0.0:8080")
 }
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
+.PHONY: build
+
+build: build_windows build_current
+	echo "Done"
 
 build_windows:
 	GOOS=windows GOARCH=amd64 go build -o ./dist/
 
-build:
+build_current:
 	go build -o ./dist/
-	
